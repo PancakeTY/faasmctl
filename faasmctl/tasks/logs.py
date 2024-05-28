756e517a609ff1caa37333425a6039330ce60e65
Tianyu Qi <tiqi@student.unimelb.edu.au>
2024-05-28 01:00:28 +0000

    feat(logs): Add Logs clean function for planner and workers
    As title. It only supports docker now, and only clean planner and workers
    logs.

--- faasmctl/tasks/logs.py
+++ faasmctl/tasks/logs.py
@@ -7,6 +7,7 @@ from faasmctl.util.config import (
 )
 from faasmctl.util.k8s import run_k8s_cmd
 from invoke import task
+import subprocess
 
 
 def get_compose_logs(s, follow, ini_file, last_restart):
@@ -71,3 +72,32 @@ def logs(ctx, s, follow=False, ini_file=None):
         get_k8s_logs(s, follow, ini_file, last_restart)
     else:
         raise RuntimeError("Unrecognised backend: {}".format(backend))
+
+@task
+def clean(ctx, ini_file=None):
+    """
+    Clean the logs of planner and workers in the docker
+
+    Parameters:
+    - ini_file (str): path to the cluster's INI file
+    """
+    if not ini_file:
+        ini_file = get_faasm_ini_file()
+    print(ini_file)
+    backend = get_faasm_ini_value(ini_file, "Faasm", BACKEND_INI_STRING)
+    if backend == COMPOSE_BACKEND:
+        clean_cmd = "echo \"\" > $(docker inspect --format='{{{{.LogPath}}}}' {})" 
+        cluster_name = get_faasm_ini_value(ini_file, "Faasm", "cluster_name")
+        planner_clean_cmd = clean_cmd.format(cluster_name+"-planner-1")
+        planner_clean_cmd = "".join(planner_clean_cmd)
+        print(planner_clean_cmd)
+        subprocess.run(planner_clean_cmd, shell=True, check=True)
+        worker_names = get_faasm_ini_value(ini_file, "Faasm", "worker_names")
+        worker_names = [p.strip() for p in worker_names.split(",") if p.strip()]
+        for name in worker_names:
+            worker_clean_cmd = clean_cmd.format(name)
+            worker_clean_cmd = "".join(worker_clean_cmd)
+            print(worker_clean_cmd)
+            subprocess.run(worker_clean_cmd, shell=True, check=True)
+    elif backend == K8S_BACKEND:
+        raise RuntimeError("K8s backend does not support cleaning logs")
